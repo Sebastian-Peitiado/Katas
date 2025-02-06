e3b78f5844f343c238cf521ee85c06c46c4d53ff
speitiado <spetiiado@auronix.com>
2025-02-06 18:46:56 -0300

    Ejercicio 2 terminado

--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -7,10 +7,12 @@ class Calculadora:
         self.numeros = numeros 
     
     def sumar(self):
-        if self.numeros == "":
-            return 0  
         
-        numeros_enteros = list(map(int, self.numeros.split(",")))
+        if self.numeros == "":
+            return 0
+        
+        
+        numeros_enteros = list(map(int, filter(None, self.numeros.split(","))))
 
         return sum(numeros_enteros)  
 
@@ -29,3 +31,8 @@ def test_prueba_un_solo_numero():
 def test_prueba_cadena_con_coma():
     calculadora = Calculadora(numeros="1,2")
     assert calculadora.sumar() == 3  
+
+def test_multiples_cadenas_con_coma():
+    calculadora = Calculadora(numeros="1,2,3")
+    assert calculadora.sumar() == 6  
+
