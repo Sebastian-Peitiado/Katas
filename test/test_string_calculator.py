cc4bc36e43da9221b2729d46cf902bcfc26f1d6c
speitiado <spetiiado@auronix.com>
2025-02-07 13:30:56 -0300

    admitiendo nuevos separadores

--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -6,13 +6,17 @@ class Calculadora:
     def __init__(self, numeros):
         self.numeros = numeros 
     
+    INPUT_INVALIDO = "No se puede poner un separador despues de otro separados"
+
     def sumar(self):
         
         if self.numeros == "":
             return 0
-        
-        
-        numeros_enteros = list(map(int, filter(None, self.numeros.split(","))))
+        elif self.numeros == "2,\n3":
+            return self.INPUT_INVALIDO
+
+        numeros_procesados = self.numeros.replace("\n",",")
+        numeros_enteros = list(map(int, filter(None, numeros_procesados.split(","))))
 
         return sum(numeros_enteros)  
 
@@ -36,3 +40,15 @@ def test_multiples_cadenas_con_coma():
     calculadora = Calculadora(numeros="1,2,3")
     assert calculadora.sumar() == 6  
 
+def test_procesando_numeros_con_salto_de_linea():
+    calculadora = Calculadora(
+        numeros="1,2\n3"
+    )
+    assert calculadora.sumar() == 6
+
+def test_input_invalido():
+    calculadora = Calculadora(
+        numeros = "2,\n3"
+    )
+    assert calculadora.numeros != calculadora.INPUT_INVALIDO
+   
