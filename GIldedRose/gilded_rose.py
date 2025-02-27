8203a3908c1c1309a8eec5f7f693ddbd51a89d3c
speitiado <spetiiado@auronix.com>
2025-02-27 12:27:42 -0300

    add variables and 1method

--- GIldedRose/gilded_rose.py
+++ GIldedRose/gilded_rose.py
@@ -10,9 +10,12 @@ class GildedRose(object):
         self.update_item(item)
     
     def update_item(self, item):
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
-                item.downgrade_item()
+        is_special_item = item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        is_not_sulfura = item.name != "Sulfuras, Hand of Ragnaros"
+        self.decrese_sell_in(item)
+        if is_special_item:
+            if item.quality > 0 and is_not_sulfura:
+                self.downgrade_item(item)
         else:
             if item.quality < 50:
                 item.quality += 1
@@ -22,15 +25,15 @@ class GildedRose(object):
                     if item.sell_in < 6 and item.quality < 50:
                         item.quality += 1
 
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in -= 1
-
         if item.sell_in < 0:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+                if item.quality > 0 and is_not_sulfura:
                     item.quality -= 1
             else:
                 item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert" else min(50, item.quality + 1)
+
+    def decrese_sell_in(self, item):
+        item.sell_in -= 1
 
     def downgrade_item(self, item):
        item.quality -= 1
