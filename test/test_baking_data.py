99b68938f2e9160824ee1aa3034fe3e47308261f
glpat-QQMxDSY2FU6zhh_6HJeG <speitiado@auronix.com>
2025-03-12 12:10:11 -0300

    escenarios de formato incorrecto y numero negativo pasando

--- test/test_baking_data.py
+++ test/test_baking_data.py
@@ -4,10 +4,16 @@ class Account ():
 
     BALANCE = 0
     INVALID_FORMAT = "Strings are not allowed"
+    NO_NEGATIVE_NUMBERS = "Negative numbers are not allowed"
 
     def deposit(self, amount):
-        self.BALANCE+= amount
-    
+        
+        if not isinstance(amount, (int, float)) :
+            raise ValueError(self.INVALID_FORMAT)
+        if amount < 0:
+            raise ValueError(self.NO_NEGATIVE_NUMBERS)
+        self.BALANCE += amount
+        
     def get_balance(self):
         return self.BALANCE
   
@@ -26,3 +32,8 @@ def test_invalid_deposit():
     account = Account()
     with pytest.raises(Exception, match=account.INVALID_FORMAT):
         account.deposit("500")
+
+def test_no_negatives_number():
+    account = Account()
+    with pytest.raises(Exception, match=account.NO_NEGATIVE_NUMBERS):
+        account.deposit(-100)
