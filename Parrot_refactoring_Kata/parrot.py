8d071470a4d9524e36b4a9a25d5267f279ab42e7
glpat-QQMxDSY2FU6zhh_6HJeG <speitiado@auronix.com>
2025-03-25 10:04:21 -0300

    creando clases para cada loro

--- Parrot_refactoring_Kata/parrot.py
+++ Parrot_refactoring_Kata/parrot.py
@@ -23,16 +23,7 @@ class Parrot:
                 return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
             case ParrotType.NORWEGIAN_BLUE:
                 return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
-
-    def cry(self):
-        match self._type:
-            case ParrotType.EUROPEAN:
-                return "Sqoork!"
-            case ParrotType.AFRICAN:
-                return "Sqaark!"
-            case ParrotType.NORWEGIAN_BLUE:
-                return "Bzzzzzz" if self._voltage > 0 else "..."
-
+            
     def _compute_base_speed_for_voltage(self, voltage):
         return min([24.0, voltage * self._base_speed()])
 
@@ -41,3 +32,22 @@ class Parrot:
 
     def _base_speed(self):
         return 12.0
+    
+
+class EuropeanParrot(Parrot):
+    def cry(self):
+        return "Sqoork!"
+    def speed(self):
+        return self._base_speed()
+
+class AfricanParrot(Parrot):
+    def cry(self):
+       return "Sqaark!"
+    def speed(self):
+        return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
+
+class NorweianBlueParrot(Parrot):
+    def cry(self):
+        return "Bzzzzzz" if self._voltage > 0 else "..."
+    def speed(self):
+        return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
