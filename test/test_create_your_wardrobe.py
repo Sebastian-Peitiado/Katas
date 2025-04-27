36e9bb73b666a5def2178e8b2fa4a0f2208c5d4f
glpat-QQMxDSY2FU6zhh_6HJeG <speitiado@auronix.com>
2025-04-27 13:02:29 -0300

    creando bucle while

--- test/test_create_your_wardrobe.py
+++ test/test_create_your_wardrobe.py
@@ -10,33 +10,60 @@
 #9 tiene que especificar que medidas utilizo para armarlo
 
 class Armario():
-    def __init__(self,placas_disponibles: list,tamaño_maximo: int, precios: dict):
+    def __init__(self,placas_disponibles: list,tamaño_maximo: int):
         self.placas_disponibles = placas_disponibles
         self.tamaño_maximo = tamaño_maximo
-        self.precios = precios
+        
 
 
 
     def armado(self):
-        resultado = []
-        plaquetas_introducidas = []
-        if self.placas_disponibles == self.tamaño_maximo:
-            plaquetas_introducidas.append(self.placas_disponibles)
-        
+        if self.tamaño_maximo == 0:
+            return [[]]
+        lista_resultados = []
+        pila = [[]]
+        print(pila)
+        while pila:
+           combinacion_a_verificar = pila.pop()
+           if combinacion_a_verificar == []:
+            total = 0
+           else:
+               total = sum(combinacion_a_verificar)
+           if total == self.tamaño_maximo:
+               lista_resultados.append(combinacion_a_verificar)
+           elif total < self.tamaño_maximo:
+               for placas in self.placas_disponibles:
+                   pila.append(combinacion_a_verificar.append(placas))
+
+               
+        #for x in range(len(self.placas_disponibles)):
+        #    if self.placas_disponibles[x] == self.tamaño_maximo:
+        #        return [[self.placas_disponibles[x]]]
+        return lista_resultados
+            
+
     
     
         
 
 
 
-def test_creacion_de_armario():
-    armario = Armario(placas_disponibles=[50,75,100,120],tamaño_maximo=250)
-    assert armario != None
+def test_esapcio_0_devuelve_lista_vacia():
+    armario = Armario(placas_disponibles=[50],tamaño_maximo=0)
+    resultado = armario.armado()
+    assert resultado == [[]]
 
-def test_armado_de_armario():
-    armario = Armario(placas_disponibles=[50,75,100,120],tamaño_maximo=250,precios={50: 59, 75: 62, 100: 90, 120: 111})
-    armario.armado()
-    assert armario.armado() == [[75,75,100], 214]
+def test_sin_solucion_devuelve_lista_vacia():
+    armario = Armario(placas_disponibles=[50],tamaño_maximo=25)
+    resultado = armario.armado()
+    assert resultado == [[]]
     
+def test_un_solo_elemento_devuelve_una_lista():
+    armario = Armario(placas_disponibles=[50],tamaño_maximo=50)
+    resultado = armario.armado()
+    assert resultado == [[50]]
 
-    
+def test_2_elementos_en_una_lista():
+    armario = Armario(placas_disponibles=[50,75],tamaño_maximo=100)
+    resultado = armario.armado()
+    assert resultado == [[50,50]]
